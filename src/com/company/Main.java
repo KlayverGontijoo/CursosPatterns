56382afa132f31fb39e1a9cb8ce91cd573d3dbe0
KlayverGontijoo <klayvergontijo@outlook.com>
2020-12-02 23:14:41 -0300

    Singleton

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        Usuario u1 = Usuario.getInstance();
+        Usuario u2 = Usuario.getInstance();
+
+
+    }
+}
